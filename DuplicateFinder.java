5c51116078227dcea96ef637fe51ad08570aea5e
Superklaas <67093947+Superklaas@users.noreply.github.com>
2020-06-22 12:17:05 +0200

    Add files via upload

--- DuplicateFinder.java
+++ DuplicateFinder.java
@@ -4,15 +4,9 @@ import java.util.Random;
 
 public class DuplicateFinder {
     public static void main(String[] args) {
-        int lengthArray = enterLengthArray();
+        int lengthArray = 10;
         int[] array = enterNumbersArray(lengthArray);
         duplicatesArray(array);
-    }
-
-    public static int enterLengthArray()    {
-        Random random = new Random();
-        int lengthArray = random.nextInt(10);
-        return lengthArray;
     }
 
     public static int[] enterNumbersArray(int lengthArray) {
@@ -23,21 +17,33 @@ public class DuplicateFinder {
         }
         System.out.println("ARRAY TO BE EXAMINED");
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + ",");
         }
+        System.out.println();
         return array;
     }
 
     public static void duplicatesArray(int[] array) {
-        int amountOfEqualities = 0;
+        int amountOfDuplicates = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = i+1; j < array.length; j++) {
                 if (array[i] == array[j]) {
-                    amountOfEqualities++;
-                    System.out.println("Duplicate number " + amountOfEqualities + ": " + array[j]);
+                    amountOfDuplicates++;
+                    System.out.println("Duplicate number " + amountOfDuplicates + ": " + array[j]);
                 }
             }
         }
     }
 
 }
+
+/*
+PROBLEM 1
+If a certain number happens to appear more than twice in the array, it shows up to often as a duplicate number.
+We need an xtra check  to know if the occurred number is already noticed as a duplicate number.
+Save all the duplicates in extendable array.
+Check per new duplicate if the number is already in the duplicate array.
+
+PROBLEM 2
+Apply this program to an array of strings.
+ */
