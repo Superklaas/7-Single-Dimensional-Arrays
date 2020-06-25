643985da16fa1713e79fa291cbd7b92a52262751
Superklaas <67093947+Superklaas@users.noreply.github.com>
2020-06-25 10:59:49 +0200

    Add files via upload

--- DuplicateFinder.java
+++ DuplicateFinder.java
@@ -1,12 +1,15 @@
 package be.vdab;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class DuplicateFinder {
     public static void main(String[] args) {
         int lengthArray = 10;
         int[] array = enterNumbersArray(lengthArray);
-        duplicatesArray(array);
+        printArray(array);
+        int[] allDuplicates = findAllDuplicates(array); //find duplicates in array and put them in a separate array
+        printDistinctDuplicates(allDuplicates);
     }
 
     public static int[] enterNumbersArray(int lengthArray) {
@@ -15,35 +18,58 @@ public class DuplicateFinder {
         for (int i = 0; i < lengthArray; i++) {
             array[i] = random.nextInt(10);
         }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
         System.out.println("ARRAY TO BE EXAMINED");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ",");
         }
         System.out.println();
-        return array;
     }
 
-    public static void duplicatesArray(int[] array) {
+    public static int[] findAllDuplicates(int[] array) {
         int amountOfDuplicates = 0;
+        int[] duplicates = new int[amountOfDuplicates];
         for (int i = 0; i < array.length; i++) {
             for (int j = i+1; j < array.length; j++) {
                 if (array[i] == array[j]) {
                     amountOfDuplicates++;
-                    System.out.println("Duplicate number " + amountOfDuplicates + ": " + array[j]);
+                    int[] duplicatesTemp = new int[amountOfDuplicates];
+                    duplicatesTemp[amountOfDuplicates-1]=array[j];
+                    for (int k = 0; k < amountOfDuplicates-1; k++) {
+                        duplicatesTemp[k] = duplicates[k];
+                    }
+                    duplicates = duplicatesTemp;
                 }
             }
         }
+        System.out.print("ALL DUPLICATES\n");
+        for (int l = 0; l < amountOfDuplicates; l++) {
+            System.out.print(duplicates[l] + " ");
+        }
+        return duplicates;
     }
 
+    public static void printDistinctDuplicates(int[] array) {
+        System.out.println("\nDISTINCT DUPLICATES");
+        for(int i=0;i<array.length;i++){
+            boolean isDistinct = false;
+            for(int j=0;j<i;j++){
+                if(array[i] == array[j]){
+                    isDistinct = true;
+                    break;
+                }
+            }
+            if(!isDistinct){
+                System.out.print(array[i]+" ");
+            }
+        }
+    }
 }
 
 /*
-PROBLEM 1
-If a certain number happens to appear more than twice in the array, it shows up to often as a duplicate number.
-We need an xtra check  to know if the occurred number is already noticed as a duplicate number.
-Save all the duplicates in extendable array.
-Check per new duplicate if the number is already in the duplicate array.
-
 PROBLEM 2
 Apply this program to an array of strings.
  */
